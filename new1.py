7c83ceffa3624161de289962d46f8352b7401c36
Kaushal Raj <132828675+kaushalprajapatii@users.noreply.github.com>
2026-01-17 16:37:43 +0530

    Update new1.py

--- new1.py
+++ new1.py
@@ -13,6 +13,9 @@ import time
 import base64
 # in new1.py or any layout file if transparency.py is alongside new1.py
 from transparency import apply_alpha_to_color
+import streamlit as st
+st.write(list(st.secrets.keys()))
+
 
 
 from login import login
